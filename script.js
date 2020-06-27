052c5947574d21326575544019a56ec40b63770e
Victoria Kee <victoriakee95@gmail.com>
2020-06-27 09:31:42 -0500

    Write main JS function

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("submit").addEventListener("click", heightConverter);
+
+function heightConverter() {
+  const feet = parseInt(document.getElementById('feet').value * 12);
+  const inches = parseInt(document.getElementById('inches').value);
+  const cm = (feet + inches) * 2.54;
+  let n = cm.toFixed(0);
+
+  n = document.getElementById('result').innerHTML
+}
